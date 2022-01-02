0de65e27ab2c913851c478c0892eede8a4ec58aa
1234 <1234>
2022-01-02 21:30:33 +0300

    commit 1

---
+++ main.py
@@ -0,0 +1,122 @@
+import sys
+import pygame
+import os
+import random
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPainter, QColor, QPixmap
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QInputDialog, QCalendarWidget
+from PyQt5.QtWidgets import QCheckBox, QLabel, QLineEdit, QVBoxLayout, QGridLayout
+import sqlite3
+import datetime as dt
+from PyQt5.QtWidgets import QTableWidgetItem, QDialog
+
+pygame.init()
+size = width, height = 1280, 720
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def start_screen():
+    intro_text = ["   ЗАСТАВКА", "",
+                  "Правила игры",
+                  "ваша задача закрыть все клетки,",
+                  "по вертикали или горизонтали или диагонали",
+                  "Для начала нажмите на пробел"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 40)
+    text_coord = height // 3
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = width // 4
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+
+
+class AboutWindow(QWidget):
+    def __init__(self):
+        super(AboutWindow, self).__init__()
+        self.setWindowTitle('О программе')
+        self.setLayout(QVBoxLayout(self))
+        self.info = QLabel(self)
+        self.info.setText('Игроки по очереди ставят на свободные клетки поля 3×3 знаки.'
+                          'Первый, выстроивший в ряд 3 своих фигуры по вертикали, горизонтали или'
+                          ' диагонали, выигрывает. Первый ход делает игрок, ставящий крестики'
+                          'Обычно по завершении партии выигравшая сторона зачёркивает чертой свои'
+                          ' три знака (нолика или крестика), составляющих сплошной ряд.')
+        self.layout().addWidget(self.info)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main_menu.ui', self)
+        self.initUI()
+        self.about_window = AboutWindow()
+
+    def about(self):
+        self.about_window.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    running = True
+    main_wnd = MainWindow()
+    start_screen()
+    main_wnd.show()
+    # player, level_x, level_y = generate_level(load_level(name_lvl))
+    while running:
+        a = None
+        ev = None
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    ev = event
+                    a = 2
+                if event.key == pygame.K_UP:
+                    ev = event
+                    a = 3
+                if event.key == pygame.K_RIGHT:
+                    ev = event
+                    a = 4
+                if event.key == pygame.K_DOWN:
+                    ev = event
+                    a = 1
+        # camera.update(player)
+        # for sprite in all_sprites:
+        # camera.apply(sprite)
+        # all_sprites.draw(screen)
+        # all_sprites.update(ev, a)
+        # player_group.draw(screen)
+        pygame.display.flip()
+        screen.fill((255, 255, 255))
+    pygame.quit()
+    sys.exit(app.exec_())
+
+# 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
